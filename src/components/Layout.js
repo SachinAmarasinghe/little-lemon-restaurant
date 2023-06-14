3d801750a83666215d5be1623fd9963a03b1f5fc
SachinAmarasinghe <sachinamarasinghe@outlook.com>
2023-06-13 20:30:04 -0400

    SEO changes

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import Seo from "./Seo";
 
 const Layout = ({ children }) => {
   return (
     <>
+      <Seo title={"Little lemon"} description={"Little lemon is a fine dine restaurant with great food and online seat reservation convinience"} image={"../images/footer"} />
       <Header />
       <main>{children}</main>
       <Footer />
